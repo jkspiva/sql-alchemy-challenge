8f01bcc4536e2d20a887527766a24717ece6f2d0
Jayce Spiva <jayce.spiva@gmail.com>
2024-06-10 06:40:38 -0700

    uploading progress

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
 # Import the dependencies.
+import numpy as np
+import pandas as pd
+import datetime as dt
 
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect, func
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
 
+#create engine to hawaii.sqlite
+engine = create_engine('sqlite:///Resources/hawaii.sqlite')
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
